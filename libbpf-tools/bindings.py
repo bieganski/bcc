5f30af90340af187246717e53fddbb4df4170e6b
Matuesz <bieganski.m@wp.pl>
2024-09-09 13:01:15 +0200

    miracle fix for 'struct bpf_obj' (defined in libbpf.c, not bpf.h). not yet in docker.

--- libbpf-tools/bindings.py
+++ libbpf-tools/bindings.py
@@ -3,6 +3,18 @@
 import gen.bpf as bpf
 import gen.libbpf as libbpf
 
+from inspect import getmembers
+from pprint import pformat
+
+x = lambda a : pformat(getmembers(a))
+
+elf_path = "./.output/uprobe.bpf.o"
+
+bpf_obj = libbpf.bpf_object__open(elf_path)
+raise ValueError(dir(bpf_obj.contents))
+
+raise ValueError(libbpf.bpf_object__load)
+
 res = libbpf.libbpf_probe_bpf_helper(bpf.BPF_PROG_TYPE_KPROBE, bpf.BPF_FUNC_map_lookup_elem, None)
 raise ValueError(res)
 
--- libbpf-tools/gen_bindings.sh
+++ libbpf-tools/gen_bindings.sh
@@ -7,28 +7,38 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 pushd $SCRIPT_DIR
 
 # Prerequsistes.
-pip3 install --upgrade pip
-pip3 install git+https://github.com/bieganski/ctypesgen
+
+# XXX
+# pip3 install --upgrade pip
+# pip3 install git+https://github.com/bieganski/ctypesgen
+
 rm -rf gen
 mkdir gen
 
 # libbpf.h (together with workaround required by 'ctypesgen')
-LIBPF_H=./bpftool/libbpf/src/libbpf.h
-file -E $LIBPF_H
-OUT=gen/libbpf.h
-echo "#define __signed__ signed" > $OUT
-cat $LIBPF_H  >> $OUT
-sed -i 's/:0;/;/g' $OUT
+LIBBPF_C=./bpftool/libbpf/src/libbpf.c
+LIBBPF_H=./bpftool/libbpf/src/libbpf.h
+file -E $LIBBPF_H
+# OUT=gen/libbpf.h
+# echo "#define __signed__ signed" > $OUT
+# cat $LIBPF_H  >> $OUT
+sed -i 's/:0;/;/g' $LIBBPF_H
 
 # bpf.h
 BPF_H=/usr/include/linux/bpf.h
 file -E $BPF_H
 OUT=gen/bpf.h
-echo "#define __signed__ signed" > $OUT
-cat $BPF_H >> $OUT
-sed -i 's/:0;/;/g' $OUT
+# echo "#define __signed__ signed" > $OUT
+# cat $BPF_H >> $OUT
+sed 's/:0;/;/g' $BPF_H > $OUT
 
 popd > /dev/null
 
-ctypesgen -I `dirname $LIBPF_H` gen/libbpf.h -l ./libbpf.so.1 > gen/libbpf.py
-ctypesgen gen/bpf.h -l ./libbpf.so.1 > gen/bpf.py
+# ctypesgen -I `dirname $LIBBPF_H` gen/libbpf.h ./bpftool/libbpf/src/libbpf.c -l ./libbpf.so.1 > gen/libbpf.py
+ctypesgen -D__signed__=signed $OUT -l ./libbpf.so.1 > gen/bpf.py
+
+ctypesgen \
+-D__signed__=signed \
+-I /home/m.bieganski/github/libbpf/include/ \
+ -l ./libbpf.so.1 \
+$LIBBPF_C $LIBBPF_H > gen/libbpf.py
