1821fe15b01eca4959d7c93402db68a44436782c
Matuesz <bieganski.m@wp.pl>
2024-09-30 18:07:51 +0200

    fix Dockerfile

--- libbpf-tools/gen_bindings.sh
+++ libbpf-tools/gen_bindings.sh
@@ -3,48 +3,61 @@
 set -eux
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-
 pushd $SCRIPT_DIR
+
+usage() {
+    echo "Usage: $0 --libbpf-root <path>"
+    exit 1
+}
+
+# Parse arguments
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        --libbpf-root)
+            LIBBPF_ROOT="$2"
+            shift 2
+            ;;
+        *)
+            echo "Unknown option: $1"
+            usage
+            ;;
+    esac
+done
+
+# Check if LIBBPF_ROOT is set
+if [[ -z "$LIBBPF_ROOT" ]]; then
+    echo "Error: --libbpf-root is a required parameter."
+    usage
+fi
 
 # Prerequsistes.
 
-# XXX
-# pip3 install --upgrade pip
-# pip3 install git+https://github.com/bieganski/ctypesgen
+pip3 install --upgrade pip
+pip3 install git+https://github.com/bieganski/ctypesgen
 
 rm -rf gen
 mkdir gen
 
 # libbpf.h (together with workaround required by 'ctypesgen')
-LIBBPF_C=/home/m.bieganski/github/libbpf/src/libbpf.c
-LIBBPF_H=/home/m.bieganski/github/libbpf/src/libbpf.h
+LIBBPF_C=$LIBBPF_ROOT/src/libbpf.c
+LIBBPF_H=$LIBBPF_ROOT/src/libbpf.h
 file -E $LIBBPF_H
-# OUT=gen/libbpf.h
-# echo "#define __signed__ signed" > $OUT
-# cat $LIBPF_H  >> $OUT
-# sed -i 's/:0;/;/g' $LIBBPF_H
 
 # bpf.h
-BPF_H=/home/m.bieganski/github/libbpf/include/uapi/linux/bpf.h
+BPF_H=$LIBBPF_ROOT/include/uapi/linux/bpf.h
 file -E $BPF_H
-OUT=gen/bpf.h
-# echo "#define __signed__ signed" > $OUT
-# cat $BPF_H >> $OUT
-# sed 's/:0;/;/g' $BPF_H > $OUT
 
 popd > /dev/null
 
-# ctypesgen -I `dirname $LIBBPF_H` gen/libbpf.h ./bpftool/libbpf/src/libbpf.c -l ./libbpf.so.1 > gen/libbpf.py
 ctypesgen -D__signed__=signed $BPF_H -l ./libbpf.so.1 > gen/bpf.py
-
 
 # -l ./libbpf.so.1 \
 ctypesgen \
 -l ./libbpf.so.1 \
 -D__signed__=signed \
 "-D__builtin_constant_p(x)='1'" \
--I /home/m.bieganski/github/libbpf/include/ \
--I /home/m.bieganski/github/libbpf/include/uapi/ \
+-I $LIBBPF_ROOT/include/ \
+-I $LIBBPF_ROOT/include/uapi/ \
 $LIBBPF_C $LIBBPF_H > gen/libbpf.py
 
 wc -l gen/libbpf.py
